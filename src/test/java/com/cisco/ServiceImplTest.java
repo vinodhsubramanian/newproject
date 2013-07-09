0a02d8d16c9ae6dcd127fcc9c87892f12ab09abf
sbayana <sangeetha_b@persistent.co.in>
2013-07-09 19:08:45 +0530

    Adding to local

--- src/test/java/com/cisco/ServiceImplTest.java
+++ src/test/java/com/cisco/ServiceImplTest.java
@@ -9,14 +9,14 @@ import com.cicso.impl.ServiceImpl;
 
 public class ServiceImplTest {
     
-  @Test(groups = { "add1" })
+  @Test(groups = { "addlocal" })
   public void testMethod1(){
 	  Service service = new ServiceImpl();
 	  int result = service.method1(1, 2);
       Assert.assertEquals(result, 3);
   }
   
-  @Test(groups = { "add1" })
+  @Test(groups = { "addlocal" })
   public void testMethod2(){
 	  Service service = new ServiceImpl();
 	  int result = service.method2(1, 2);
