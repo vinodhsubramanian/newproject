2b82c251b32caaeefaf9387ed9307b94e19a9be7
sbayana <sangeetha_b@persistent.co.in>
2013-07-09 17:22:24 +0530

    added Test Groups

--- src/test/java/com/cisco/ServiceImplTest.java
+++ src/test/java/com/cisco/ServiceImplTest.java
@@ -8,42 +8,39 @@ import com.cicso.api.Service;
 import com.cicso.impl.ServiceImpl;
 
 public class ServiceImplTest {
-  @Test
-  public void f() {
-  }
-  
-  @Test
+    
+  @Test(groups = { "add" })
   public void testMethod1(){
 	  Service service = new ServiceImpl();
 	  int result = service.method1(1, 2);
       Assert.assertEquals(result, 3);
   }
   
-  @Test
+  @Test(groups = { "add" })
   public void testMethod2(){
 	  Service service = new ServiceImpl();
 	  int result = service.method2(1, 2);
       Assert.assertEquals(result, 3);
   }
-  @Test
+  @Test(groups = { "multiple" })
   public void testMethod3(){
 	  Service service = new ServiceImpl();
 	  int result = service.method3(1, 2, 1);
       Assert.assertEquals(result, 2);
   }
-  @Test
+  @Test(groups = { "multiple" })
   public void testMethod4(){
 	  Service service = new ServiceImpl();
 	  int result = service.method4(1, 2, 1);
       Assert.assertEquals(result, 2);
   }
-  @Test
+  @Test(groups = { "custom" })
   public void testMethod5(){
 	  Service service = new ServiceImpl();
 	  int result = service.method5(1, 2, 1);
       Assert.assertTrue(true);
   }
-  @Test
+  @Test(groups = { "custom" })
   public void testMethod6(){
 	  Service service = new ServiceImpl();
 	  int result = service.method6(1, 2, 1);
